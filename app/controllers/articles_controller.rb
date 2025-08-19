3592a192ed54b9bc9efb6eaaf80c09c4455acac3
carmarchant1986 <carlosmarchantsanchez@yahoo.es>
2025-08-19 16:13:09 -0400

    Clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,56 +7,44 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1 or /articles/1.json
-  def show
-    byebug
-    @article = Article.find(params[:id])
+  def show    
   end
 
   # GET /articles/new
-  def new
+  def new 
     @article = Article.new
   end
 
   # GET /articles/1/edit
-  def edit
+  def edit    
   end
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article = Article.new(article_params)   
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
     end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+  def update    
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_path, notice: "Article was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    set_article # find the article
+    @article.destroy                      # destroy the article
+    redirect_to articles_path    # redirect to the article list
   end
 
   private
@@ -68,5 +56,6 @@ class ArticlesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def article_params
       params.expect(article: [ :title, :description ])
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
+  #resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  #resources :articles
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
